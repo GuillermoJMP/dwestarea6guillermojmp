c094baf334d0acc5e7c6915b80998c5d020268b7
GuillermoJMP <scs82358@educastur.es>
2025-03-17 10:13:54 +0100

    Mejoras de funcionalidades
    Enlaces a inicio creados

--- src/main/java/com/dwes/Dwestarea6guillermojmpApplication.java
+++ src/main/java/com/dwes/Dwestarea6guillermojmpApplication.java
@@ -13,18 +13,25 @@ public class Dwestarea6guillermojmpApplication {
 		SpringApplication.run(Dwestarea6guillermojmpApplication.class, args);
 
 		try {
+			String url = "http://localhost:8081/inicio";
+			System.out.println("\n\t\t\t\033[1;36mLink de Entrada --> " + url + "\033[0m");
 
-			String ip = InetAddress.getLocalHost().getHostAddress();
-			String port = "8081";
-
-			System.out.println("\n--------------------------------------------------");
-			System.out.println("🚀 Aplicación iniciada en:");
-			System.out.println("🌍 Local:     \u001B[34mhttp://localhost:" + port + "\u001B[0m");
-			System.out.println("🌎 Red:       \u001B[34mhttp://" + ip + ":" + port + "\u001B[0m");
-			System.out.println("--------------------------------------------------\n");
-
-		} catch (UnknownHostException e) {
-			System.err.println("⚠ No se pudo obtener la IP local.");
+			ProcessBuilder pb = new ProcessBuilder();
+			String os = System.getProperty("os.name").toLowerCase();
+			if (os.contains("win")) {
+				pb.command("cmd", "/c", "start", url);
+			} else if (os.contains("mac")) {
+				pb.command("open", url);
+			} else if (os.contains("nix") || os.contains("nux")) {
+				pb.command("xdg-open", url);
+			} else {
+				throw new UnsupportedOperationException("Sistema operativo no soportado para abrir navegadores.");
+			}
+			pb.start();
+			System.out.println("\n\t\t\t\033[1;32mAbriendo el navegador con el enlace...\033[0m");
+		} catch (Exception e) {
+			System.out.println("\n\t\t\t\033[1;31mError al intentar abrir el navegador:\033[0m");
+			e.printStackTrace();
 		}
 	}
 }
